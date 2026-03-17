16a2ce7657f82304db7af07cba2b06962ef4c141
Sneex17 <228738464+Sneex17@users.noreply.github.com>
2026-03-17 17:06:17 -0400

    Arreglo de Bugs

--- A2BankingServidor/CPresentacion/obj/Debug/net8.0-windows/CPresentacion.AssemblyInfo.cs
+++ A2BankingServidor/CPresentacion/obj/Debug/net8.0-windows/CPresentacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CPresentacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+175c16c4565925fec91536b5fdd9d94925ba77e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e66a1d7700e42fd74b6bb070d603dc8ffb4fe6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CPresentacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CPresentacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
