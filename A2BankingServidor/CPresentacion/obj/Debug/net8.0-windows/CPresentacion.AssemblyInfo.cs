3f3c3fa69487650813934ebc2695e38561301b3b
Sneex17 <228738464+Sneex17@users.noreply.github.com>
2026-03-17 22:29:38 -0400

    Actualización de la rama

--- A2BankingServidor/CPresentacion/obj/Debug/net8.0-windows/CPresentacion.AssemblyInfo.cs
+++ A2BankingServidor/CPresentacion/obj/Debug/net8.0-windows/CPresentacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CPresentacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1edbd3b227a58db5dea80064a6173f748fcb4e21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed0a7ea8f48aac557793d24c7bfb5154320c956a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CPresentacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CPresentacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
