6d27a09b93139437a098573a1aa4bc78fd9ec6cf
Sneex17 <228738464+Sneex17@users.noreply.github.com>
2026-03-23 18:04:24 -0400

    Diseño del cajero (cliente)

--- A2BankingCliente/CPresentacion/obj/Debug/net8.0-windows/CPresentacion.AssemblyInfo.cs
+++ A2BankingCliente/CPresentacion/obj/Debug/net8.0-windows/CPresentacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CPresentacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14ed14604d32ee0950bf161c315c603655fe8b63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6ed231ddb2360811e9b9bb228536c3f8fab61b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CPresentacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CPresentacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
